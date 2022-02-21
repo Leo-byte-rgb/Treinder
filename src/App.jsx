6c1b1dabd09a3e7145e82cb63c1a264c93fa627f
leo-byte-rgb <leonardoagostininho@gmail.com>
2022-02-21 15:36:28 -0300

    Feat trainers screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import { Home } from "./pages/Home";
 import { UserSignUp } from "./pages/UserSignUp";
 import { TrainerSignUp } from "./pages/TrainerSignUp";
-
+import { Trainers } from "./pages/Trainers";
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <ChakraProvider>
-      <TrainerSignUp />
+      <BrowserRouter>
+        <Trainers />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
